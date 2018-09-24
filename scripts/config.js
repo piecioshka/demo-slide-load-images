584f8ca7e11515afc72182e97770f55c0ab9de62
Piotr Kowalski <piecioshka@gmail.com>
2018-09-24 18:36:03 +0200

    Update placeholder service

--- scripts/config.js
+++ scripts/config.js
@@ -5,10 +5,10 @@ config = this.config || {};
 
 // array with source to example images
 config.images = [
-    "http://beerhold.it/400/400?1",
-    "http://beerhold.it/400/400?2",
-    "http://beerhold.it/400/400?3",
-    "http://beerhold.it/400/400?4"
+    "https://via.placeholder.com/400x400?text=1",
+    "https://via.placeholder.com/400x400?text=2",
+    "https://via.placeholder.com/400x400?text=3",
+    "https://via.placeholder.com/400x400?text=4"
 ];
 
 // html tag id
